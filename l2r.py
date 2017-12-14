b3a846026607ea79adc763732867ffd18faaa3fc
Kirill Konevets <kkonevets@gmail.com>
2017-12-14 19:56:58 +0300

    all

--- l2r.py
+++ l2r.py
@@ -2,4 +2,4 @@ from common import *
 
 '../data/train.txt'
 
-java -jar RankLib.jar -train train.txt -validate vali.txt -ranker 6 -metric2t MAP -save rl_lmart.txt
+java -jar RankLib.jar -train train.txt -validate vali.txt -ranker 6 -metric2t MAP -save rl_lmart.txt -norm zscore
