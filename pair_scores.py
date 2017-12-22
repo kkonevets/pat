1880c921d46eddd6fe2312043223bd6fe631880e
Kirill Konevets <kkonevets@gmail.com>
2017-12-22 19:57:18 +0300

    all

--- pair_scores.py
+++ pair_scores.py
@@ -2,7 +2,7 @@
 from common import *
 from itertools import *
 import qdr
-from operator import itemgetter
+from operator import itemgetter, attrgetter
 from scipy.spatial import distance
 from importlib import reload
 from functools import partial
@@ -49,58 +49,48 @@ class Data:
             self.all_mpk = pickle.load(f)
         self.mpk = ft.MPK()
 
-    @staticmethod
-    def read_json(filename):
+    def _push_worker(self, filename):
         with GzipFile(filename) as f:
             data = ujson.load(f)
-        return data
-
-    def _push_worker(self, data):
+        common_keys = self.ids.intersection(data.keys())
         _docs = {_id: {k: [self.token2id[w] for s in v for w in s]
-                       for k, v in doc.items() if _id in self.ids} for _id, doc in data.items()}
+                       for k, v in data[_id].items()}
+                 for _id in common_keys}
         return _docs
 
     def push_docs_to_ram(self):
         start = time.time()
         docs_ram = {}
 
-        for chunk in grouper(self.corpus_files, cpu_count):
-            with multiprocessing.Pool(processes=cpu_count) as pool:
-                res = pool.map(self.read_json, chunk)
-            with ThreadPool(processes=cpu_count) as pool:
-                res = pool.map(self._push_worker, res)
-            for doc in res:
-                docs_ram.update(doc)
+        for filename in tqdm(self.corpus_files):
+            docs_ram.update(self._push_worker(filename))
 
         end = time.time()
         logging.info('docs loaded in %f m.' % ((end - start)/60.))
-        gc.collect()
         return docs_ram
 
     @staticmethod
-    def jaccard(d1, d2):
-        keys = set(d1.keys()).intersection(d2.keys())
-        jac = {}
-        for k in keys:
-            s1 = set(d1[k])
-            s2 = set(d2[k])
-            lu = len(s1.union(s2))
-            if lu:
-                j = len(s1.intersection(s2)) / lu
-                jac['%s_j' % k] = j
-        return jac
+    def keywise(func, *args, prefix=''):
+        keys = [set(d.keys()) for d in args]
+        keys = set.intersection(*keys)
+        _ft = {'%s_%s' % (k, prefix): func(*(d[k] for d in args)) for k in keys}
+        _ft = {'%s_%s' % (k, ki): vi for k,v in _ft.items() for ki, vi in v.items()}
+        return _ft
 
     @staticmethod
-    def independent(doc, prefix='d'):
-        _ft = {}
-        _l = 0
-        for k, words in doc.items():
-            tag = '%s_%s' % (k, prefix)
-            _ft[tag] = len(words)
-            _ft['unique_%s' % tag] = len(set(words))
-            _l += _ft[tag]
-        _ft['total_len'] = _l
-        return _ft
+    def jaccard(v1, v2):
+        s1 = set(v1)
+        s2 = set(v2)
+        lu = len(s1.union(s2))
+        if lu:
+            j = len(s1.intersection(s2)) / lu
+        else:
+            j = 0
+        return {'j': j}
+
+    @staticmethod
+    def independent(words):
+        return {"all": len(words), "unique": len(set(words))}
 
     @staticmethod
     def mean_vector(vectors):
@@ -109,15 +99,21 @@ class Data:
         else:
             return vectors
 
+    def words2vec(self, words):
+        words = [w for w in words if w in self.wv]
+        if len(words):
+            word_vecs = self.wv[words]
+            vec = self.mean_vector(word_vecs)
+            return vec
+
     def tag_vectors(self, doc):
-        vecs = {}
-        for k, v in doc.items():
-            words = [vi for vi in v if vi in self.wv]
-            if len(words):
-                word_vecs = self.wv[words]
-                vec = self.mean_vector(word_vecs)
-                vecs[k] = vec
+        vecs = {k: self.words2vec(v) for k, v in doc.items()}
+        vecs = {k: v for k,v in vecs.items() if v is not None}
         return vecs
+
+    @staticmethod
+    def cosine(v1, v2):
+        return {'cos': distance.cosine(v1, v2)}
 
     def doc_info(self, d_ix):
         info = {}
@@ -139,32 +135,14 @@ class Data:
         d_text = (w.encode() for w in chain.from_iterable(d_words.values()))
 
         ftrs.update(self.qdr.score(q_text, d_text))
-
-        ftrs.update(self.independent(q_words, prefix='q'))
-        ftrs.update(self.independent(d_words, prefix='d'))
-
-        ftrs.update(self.jaccard(q_words, d_words))
-
-        q_vecs = q['w2v']
-        d_vecs = d['w2v']
-        for k in set(q_vecs.keys()).intersection(d_vecs.keys()):
-            ftrs['%s_cos' % k] = distance.cosine(q_vecs[k], d_vecs[k])
+        ftrs.update(self.keywise(self.independent, q_words, prefix='q'))
+        ftrs.update(self.keywise(self.independent, d_words, prefix='d'))
+        ftrs.update(self.keywise(self.jaccard, q_words, d_words))
+        ftrs.update(self.keywise(self.cosine, q['w2v'], d['w2v']))
 
         n = self.mpk.compare_mpk(q['mpk'], d['mpk'], self.mpk.way)
         ftrs['mpk'] = n
 
-        return ftrs
-
-    def scores(self, samples):
-        ftrs = []
-        for anc, pos, neg in tqdm(samples):
-            q = self.doc_info(anc)
-            ranks = [1] * len(pos) + [0] * len(neg)
-            for rank, d_ix in zip(ranks, pos):
-                d = data.doc_info(d_ix)
-                _ft = data.score(q, d)
-                _ft['rank'] = rank
-                ftrs.append(_ft)
         return ftrs
 
 
@@ -195,12 +173,15 @@ all_ids = fc.load_keys('../data/keys.json')
 tfidf_blob = ft.TfIdfBlob(corpus, tfidf, index, all_ids)
 tfidf_scores = tfidf_blob.extract(samples, '../data/tfidf.csv', n_chunks=150)
 
+
+
+# all_ids = fc.load_keys('../data/keys.json')
 # q_ix = all_ids.index('5984b7c2b6b1132856638528')
 # d_ix = all_ids.index('5984b65cb6b1131291638512')
 #
-# data = Data([q_ix, d_ix], all_ids, corpus_files)
+# data = Data([q_ix, d_ix])
 #
 # q = data.doc_info(q_ix)
 # d = data.doc_info(d_ix)
 #
-# data.scores(q, d)
+# data.score(q, d)
